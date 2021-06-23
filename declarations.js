4505715d7a70fc74e5b330c3fe7a03b1490209a3
ManonHofman <84331614+ManonHofman@users.noreply.github.com>
2021-06-23 14:48:07 +0200

    Add files via upload

---
+++ declarations.js
@@ -0,0 +1,29 @@
+// declarations
+function add(number1, number2) {
+    return number1 + number2;
+  }
+  
+  add(5, 6); // 11
+
+
+//expressions
+  const add = function (number1, number2) {
+    return number1 + number2;
+  };
+  
+  add(5, 6); // 11
+
+  //arrow function
+  const add = (number1, number2) => {
+    return number1 + number2;
+  };
+  add(5, 6); // 11
+
+
+  function squared(2x2){console.log (squared)
+}
+function squared2(4x4){console.log (squared2)
+}
+const add = (squared, squared2) => {
+    return squared + squared2;
+    
